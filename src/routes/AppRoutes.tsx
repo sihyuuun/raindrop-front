530e68e04801ead9f566184bcc9f96cdc6ea9068
sihyuuun <leesh3597@gmail.com>
2025-03-27 16:16:33 +0900

    #3 feat: Cloud 페이지 라우트 추가 및 의존성 파일 정리

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import ApiTester from "@/test/pages/ApiTester.tsx";
 import KakaoAuthHandler from "@/api/KakaoAuthHandler.tsx";
+import Cloud from "@/components/cloud";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<ApiTester />} />
       <Route path="/auth/login/kakao" element={<KakaoAuthHandler />} />
+      <Route path="/cloud" element={<Cloud />} />
     </Routes>
   );
 };
