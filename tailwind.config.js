ba0bacfec3e3f989617870bee84c0ea8f3f11532
silverain <ekdldkaa@naver.com>
2025-03-27 13:31:39 +0900

    feat : reset font setting

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ["class"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        /* 애니메이션 키프레임... */
+      },
+      animation: {
+        /* 애니메이션... */
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
