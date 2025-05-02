61653f74bb03011b1c4acc59e3d6eaf52c6c7ccb
jihoi0615 <pjhalice0615@gmail.com>
2025-05-02 16:48:52 +0900

    [FEAT]POST_ Scene API요청

---
+++ src/apis/api/post/usePostScenes.ts
@@ -0,0 +1,27 @@
+import { useMutation } from "@tanstack/react-query";
+import { authClient } from "../../client";
+import { SceneRequest, SceneResponse } from "@/types/scene.types";
+
+/**
+ * Scene을 생성하는 커스텀 훅입니다.
+ *
+ * - `mutationFn`: /scene 엔드포인트로 POST 요청을 보냅니다.
+ * - `onSuccess`: 생성된 암호화 Scene ID 정보를 반환합니다.
+ *
+ * @returns react-query의 useMutation 객체. mutate 함수로 요청을 실행할 수 있습니다.
+ */
+export const usePostScenes = () => {
+  return useMutation({
+    mutationKey: ["postScene"],
+    mutationFn: async (sceneData: SceneRequest) => {
+      const { data } = await authClient.post("/scenes", sceneData);
+      return data as SceneResponse;
+    },
+    onError: (error) => {
+      console.error("Scene 생성 실패", error);
+    },
+    onSuccess: (data) => {
+      console.log("Scene 생성 성공", data);
+    },
+  });
+};
