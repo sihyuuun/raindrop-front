0c9c5dacbe92308bef7d98eba648a5984dbb5fd5
jihoi0615 <pjhalice0615@gmail.com>
2025-05-07 10:52:28 +0900

    #11 feat: PUT_Scene API요청

---
+++ src/apis/api/put/usePutScenesTheme.ts
@@ -0,0 +1,19 @@
+import { useMutation } from "@tanstack/react-query";
+import { authClient } from "../../client";
+import { SceneThemeUpdateRequest, SceneResponse } from "@/types/scene.types";
+
+export const usePutSceneTheme = () => {
+  return useMutation({
+    mutationKey: ["putSceneTheme"],
+    mutationFn: async (body: SceneThemeUpdateRequest) => {
+      const { data } = await authClient.put("/scenes/theme", body);
+      return data as SceneResponse;
+    },
+    onError: (error) => {
+      console.error("테마 수정 실패", error);
+    },
+    onSuccess: (data) => {
+      console.log("테마 수정 성공", data);
+    },
+  });
+};
---
+++ src/apis/api/put/usePutScenesVisibility.ts
@@ -0,0 +1,18 @@
+import { useMutation } from "@tanstack/react-query";
+import { authClient } from "../../client";
+import { SceneUpdateVisibilityRequest } from "@/types/scene.types";
+
+export const usePutSceneVisibility = () => {
+  return useMutation({
+    mutationKey: ["putSceneVisibility"],
+    mutationFn: async (body: SceneUpdateVisibilityRequest) => {
+      await authClient.put("/scenes/visibility", body);
+    },
+    onError: (error) => {
+      console.error("공개 상태 수정 실패", error);
+    },
+    onSuccess: () => {
+      console.log("공개 상태 수정 성공");
+    },
+  });
+};
