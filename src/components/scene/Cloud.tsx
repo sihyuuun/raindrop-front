b15ebbc5510802f4a22b16b5de4c707b951711f6
sihyuuun <leesh3597@gmail.com>
2025-05-08 16:50:08 +0900

    #29 feat: Cloud 컴포넌트에서 날씨 상태에 따른 RainLayer 렌더링 로직 추가

--- src/components/scene/Cloud.tsx
+++ src/components/scene/Cloud.tsx
@@ -1,12 +1,18 @@
 import { Canvas } from "@react-three/fiber";
 import { Environment } from "@react-three/drei";
 import { DEFAULT_ENVIRONMENT_PRESET } from "@/lib/constants";
+import RainLayer from "./RainLayer";
+import { useWeatherQuery } from "@/hooks/useWeatherQuery";
+import { isRainy } from "@/utils/weatherUtils";
 
 export default function Cloud() {
+  const { data: weather, isLoading } = useWeatherQuery();
+
   return (
     <div className="w-full h-full">
       <Canvas camera={{ fov: 75, position: [0, 0, 5] }}>
         <Environment preset={DEFAULT_ENVIRONMENT_PRESET} background blur={1} />
+        {!isLoading && weather?.id && isRainy(weather.id) && <RainLayer />}
       </Canvas>
     </div>
   );
