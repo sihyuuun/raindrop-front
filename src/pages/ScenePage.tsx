f7b0decbfff731ba528204c499ebd28b6024d22e
silverain <ekdldkaa@naver.com>
2025-05-08 10:14:40 +0900

    #25 feat : scene정보 조회 및 UI반영

--- src/pages/ScenePage.tsx
+++ src/pages/ScenePage.tsx
@@ -1,10 +1,23 @@
+// import { useGetScenes } from "@/apis/api/get/useGetScenes";
+import { useGetEncryptedSceneIds } from "@/apis/api/get/useGetEncryptedSceneIds";
 import { ButtonLg } from "@/components/scene/ButtonLg";
 import Cloud from "@/components/scene/Cloud";
 import { ProfileHeader } from "@/components/scene/ProfileHeader";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export const ScenePage = () => {
   const { encryptedSceneId } = useParams<{ encryptedSceneId: string }>();
+  const { isSuccess, data } = useGetEncryptedSceneIds(encryptedSceneId ?? "");
+  const [userData, setUserData] = useState({ nickName: "", profileImage: "" });
+  useEffect(() => {
+    if (isSuccess) {
+      setUserData({
+        nickName: data.data.ownerNickname,
+        profileImage: data.data.ownerProfileImage,
+      });
+    }
+  }, [isSuccess]);
 
   return (
     <div className="relative w-screen h-screen overflow-hidden">
@@ -13,7 +26,8 @@ export const ScenePage = () => {
       </div>
 
       <div className="relative z-10 flex flex-col h-full justify-between px-[5%] py-[5%]">
-        <ProfileHeader />
+        <ProfileHeader userData={userData} />
+        <div>{encryptedSceneId}</div>
         <ButtonLg />
       </div>
     </div>
