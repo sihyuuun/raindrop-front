0a29b3bebe44335809037f6794cbe96a4ba93631
sihyuuun <leesh3597@gmail.com>
2025-05-09 15:40:04 +0900

    #35 refactor: 카카오 공유하기 함수 분리 및 Owner 여부에 따라 버튼 클릭 시 카톡 공유 혹은 메시지로 navigate 기능 추가

--- src/components/scene/ButtonLg.tsx
+++ src/components/scene/ButtonLg.tsx
@@ -1,54 +1,16 @@
 import { Button } from "@/components/ui/button";
-import { useEffect } from "react";
-const { Kakao } = window;
 
-export function ButtonLg({ isOwner }: { isOwner: boolean }) {
-  useEffect(() => {
-    const key = import.meta.env.VITE_KAKAO_JAVASCRIPT_KEY;
+interface ButtonLgProps {
+  isOwner: boolean;
+  onClick?: () => void;
+}
 
-    Kakao.cleanup();
-    Kakao.init(key);
-    console.log(Kakao.isInitialized());
-  }, []);
-
-  const shareKakao = () => {
-    Kakao.Share.sendDefault({
-      objectType: "feed",
-      content: {
-        title: "💧빗속말💧",
-        description: "비 오는 날, 상대방에게 보여줄 메시지를 남겨보세요.",
-        imageUrl: "https://k.kakaocdn.net/14/dn/btsMK8QqGc4/MZkDF42K4dHsItG5TeYkTK/o.jpg",
-        link: {
-          mobileWebUrl: location.href,
-          webUrl: location.href,
-        },
-      },
-
-      buttons: [
-        {
-          title: "버블 남기러 가기",
-          link: {
-            mobileWebUrl: location.href,
-            webUrl: location.href,
-          },
-        },
-      ],
-    });
-  };
-
-  const handleClick = () => {
-    if (isOwner) {
-      shareKakao();
-    } else {
-      // 버블 남기는 메시지 페이지로 이동
-    }
-  };
-
+export function ButtonLg({ isOwner, onClick }: ButtonLgProps) {
   const BUTTON_TEXT = isOwner ? "🫧 버블 공유하기" : "🫧 버블 남기기";
 
   return (
     <Button
-      onClick={handleClick}
+      onClick={onClick}
       className="bg-gray-50 font-jua rounded-2xl mb-[10%] h-[3rem] text-xl shadow-lg cursor-pointer">
       {BUTTON_TEXT}
     </Button>
--- src/pages/ScenePage.tsx
+++ src/pages/ScenePage.tsx
@@ -1,33 +1,37 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useGetEncryptedSceneIds } from "@/apis/api/get/useGetEncryptedSceneIds";
 import { ButtonLg } from "@/components/scene/ButtonLg";
 import { useAuthStore } from "@/store/authStore";
 import { SceneLayout } from "@/components/scene/SceneLayout";
+import { useKakaoShare } from "@/hooks/useKakaoShare";
 
 export const ScenePage = () => {
   const { encryptedSceneId } = useParams<{ encryptedSceneId: string }>();
+  const navigate = useNavigate();
+
   const { isSuccess, data } = useGetEncryptedSceneIds(encryptedSceneId ?? "");
   const { user, isAuthenticated } = useAuthStore();
+  const shareToKakao = useKakaoShare();
 
   const [isOwner, setIsOwner] = useState(false);
 
   useEffect(() => {
     //owner, guest 신분 분기 처리
-    if (
-      isSuccess &&
-      isAuthenticated &&
-      user?.email === data.data.ownerSocialId
-    ) {
+    if (isSuccess && isAuthenticated && user?.email === data.data.ownerSocialId) {
       setIsOwner(true);
     }
   }, [isSuccess, data, isAuthenticated]);
 
   if (!encryptedSceneId) return null;
 
+  const handleLeaveMessage = () => {
+    navigate(`/message?id=${encryptedSceneId}`);
+  };
+
   return (
     <SceneLayout encryptedSceneId={encryptedSceneId}>
-      <ButtonLg isOwner={isOwner} />
+      <ButtonLg isOwner={isOwner} onClick={isOwner ? shareToKakao : handleLeaveMessage} />
     </SceneLayout>
   );
 };
