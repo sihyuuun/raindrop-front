19c6730d4957cbc2678c7da0352988e8d30b8998
jihoi0615 <pjhalice0615@gmail.com>
2025-05-13 11:04:22 +0900

    #42 feat: ScenePage develop 상태로 복구

--- src/pages/ScenePage.tsx
+++ src/pages/ScenePage.tsx
@@ -5,9 +5,6 @@ import { ButtonLg } from "@/components/scene/ButtonLg";
 import { useAuthStore } from "@/store/authStore";
 import { SceneLayout } from "@/components/scene/SceneLayout";
 import { useKakaoShare } from "@/hooks/useKakaoShare";
-import { useModalStore } from "@/store/modalstore";
-import { ModalThemeSelector } from "@/components/common/ModalThemeSelector";
-import { EnvironmentPreset } from "@/lib/constants";
 
 export const ScenePage = () => {
   const { encryptedSceneId } = useParams<{ encryptedSceneId: string }>();
@@ -19,14 +16,10 @@ export const ScenePage = () => {
 
   const [isOwner, setIsOwner] = useState(false);
 
-  const [themePreset, setThemePreset] = useState<EnvironmentPreset>("sunset");
-
-  const { isOpen, openModal, closeModal } = useModalStore();
-
   useEffect(() => {
+    //owner, guest 신분 분기 처리
     if (isSuccess && isAuthenticated && user?.email === data.data.ownerSocialId) {
       setIsOwner(true);
-      openModal("sceneTheme"); // 페이지 로딩 시 모달 띄움
     }
   }, [isSuccess, data, isAuthenticated]);
 
@@ -37,19 +30,8 @@ export const ScenePage = () => {
   };
 
   return (
-    <SceneLayout encryptedSceneId={encryptedSceneId} themePreset={themePreset}>
+    <SceneLayout encryptedSceneId={encryptedSceneId}>
       <ButtonLg isOwner={isOwner} onClick={isOwner ? shareToKakao : handleLeaveMessage} />
-      {isOpen("sceneTheme") && (
-        <ModalThemeSelector
-          animateIn={true}
-          onClose={() => closeModal("sceneTheme")}
-          onSave={(preset) => {
-            setThemePreset(preset);
-            closeModal("sceneTheme");
-          }}
-          onPreview={(preset) => setThemePreset(preset)}
-        />
-      )}
     </SceneLayout>
   );
 };
