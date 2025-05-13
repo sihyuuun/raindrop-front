d980826e7a00af6eb8853d98b536b0e86ff26acf
sihyuuun <leesh3597@gmail.com>
2025-05-13 11:17:26 +0900

    #39 chore: 카카오 로그인 버튼 스타일 관련  오타 삭제

--- src/components/common/ModalLoginPrompt.tsx
+++ src/components/common/ModalLoginPrompt.tsx
@@ -26,7 +26,7 @@ export const ModalLoginPrompt = ({ onClose, animateIn, onLogin }: ModalLoginProm
               </h2>
               <button
                 onClick={onLogin}
-                className="rounded-full px-6 py-2 text-black font-semibold mt-1 hover:opacity-90 shadow cursor-pointer bg-"
+                className="rounded-full px-6 py-2 text-black font-semibold mt-1 hover:opacity-90 shadow cursor-pointer"
                 style={{ backgroundColor: "#FEE500" }}>
                 카카오로 로그인
               </button>
