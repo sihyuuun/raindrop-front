44e17200b8c94f3ce7ae12870a5e4f4b74f05bbc
jihoi0615 <pjhalice0615@gmail.com>
2025-05-14 14:45:50 +0900

    #62 feat: ScenePage 수정사항

--- src/pages/ScenePage.tsx
+++ src/pages/ScenePage.tsx
@@ -5,6 +5,12 @@ import { ButtonLg } from "@/components/scene/ButtonLg";
 import { useAuthStore } from "@/store/authStore";
 import { SceneLayout } from "@/components/scene/SceneLayout";
 import { useWebShare } from "@/hooks/useWebShare";
+import { Button } from "@/components/ui/button";
+import { Modal } from "@/components/common/Modal";
+import { useModalStore } from "@/store/modalstore";
+import { usePutScenesTheme } from "@/apis/api/put/usePutScenesTheme";
+import { EnvironmentPreset } from "@/lib/constants";
+import { useSceneStore } from "@/store/sceneStore";
 
 export const ScenePage = () => {
   const { encryptedSceneId } = useParams<{ encryptedSceneId: string }>();
@@ -15,11 +21,31 @@ export const ScenePage = () => {
   const shareToLink = useWebShare();
 
   const [isOwner, setIsOwner] = useState(false);
+  const { openModal } = useModalStore();
+  const { mutate: putTheme } = usePutScenesTheme();
+  const { setPreset } = useSceneStore();
 
+  const handleSaveTheme = (preset: EnvironmentPreset) => {
+    if (data?.data?.sceneId) {
+      putTheme({
+        sceneId: data.data.id,
+        theme: preset,
+      });
+    }
+  };
   useEffect(() => {
     //owner, guest 신분 분기 처리
-    if (isSuccess && isAuthenticated && user?.email === data.data.ownerSocialId) {
+    if (
+      isSuccess &&
+      isAuthenticated &&
+      data?.data?.ownerSocialId &&
+      user?.email === data.data.ownerSocialId
+    ) {
       setIsOwner(true);
+    }
+    // preset
+    if (isSuccess) {
+      setPreset(data.data.theme);
     }
   }, [isSuccess, data, isAuthenticated]);
 
@@ -28,13 +54,18 @@ export const ScenePage = () => {
   const handleLeaveMessage = () => {
     navigate(`/message?id=${encryptedSceneId}`);
   };
-
+  const handleOpenThemeModal = () => {
+    openModal("themeModal");
+  };
   return (
     <SceneLayout
       encryptedSceneId={encryptedSceneId}
+      preset={data?.data?.theme as EnvironmentPreset}
       // 2D UI 요소 (ButtonLg)를 일반 children으로 전달
       children={
         <div className="flex flex-col h-full justify-end">
+          {isOwner && <Button onClick={handleOpenThemeModal}>버튼</Button>}
+          <Modal modalKey="themeModal" onSave={handleSaveTheme} />
           <ButtonLg isOwner={isOwner} onClick={isOwner ? shareToLink : handleLeaveMessage} />
         </div>
       }
