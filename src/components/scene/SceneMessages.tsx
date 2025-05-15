85af130e06d20af4d95d4255fd77fc3ddb50b4b6
sihyuuun <leesh3597@gmail.com>
2025-05-15 16:36:27 +0900

    #82 feat: 고정 위치 수정 및 메시지 페이지 FloatingMessageBubble 관련 에러 수정

--- src/components/scene/SceneMessages.tsx
+++ src/components/scene/SceneMessages.tsx
@@ -13,16 +13,16 @@ const modelComponents: Record<ModelId, BubbleComponentType> = {
 };
 
 const fixedPositions: [number, number, number][] = [
-  [-1, 2.5, 0],
-  [1.2, 2, 0],
-  [-0.4, 0.7, 0],
-  [0.4, 0.4, 0],
-  [0, 0.1, 0],
-  [-0.5, -0.2, 0],
-  [0.5, -0.4, 0],
-  [-0.4, -0.6, 0],
-  [0.4, -0.75, 0],
-  [0, -2, 0],
+  [-1.0, 2.4, 0],
+  [-0.2, 1.5, 0],
+  [1.0, 2.2, 0],
+  [-0.9, 0.5, 0],
+  [0.8, 0.3, 0],
+  [-0.1, -0.4, 0],
+  [0.5, -1.4, 0],
+  [1.0, -2.4, 0],
+  [-0.6, -2.3, 0],
+  [-1.2, -1.5, 0],
 ];
 
 export const SceneMessages = ({
@@ -38,7 +38,7 @@ export const SceneMessages = ({
 
   return (
     <>
-      {messageData.data?.map((msg: MessageResponse, index: number) => {
+      {messageData.data?.slice(0, 10).map((msg: MessageResponse, index: number) => {
         const BubbleComponent = modelComponents[msg.modelId];
         const position = fixedPositions[index];
 
