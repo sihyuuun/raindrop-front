9f30da4b1b785c05c77627d1f8b8b61a5fb56f8a
pillow12360 <ehdcks1256@naver.com>
2025-05-16 17:12:51 +0900

    #101 design: 배경색 변경

--- src/main.tsx
+++ src/main.tsx
@@ -66,7 +66,7 @@ const MobileViewportWrapper = () => {
 
   return (
     <div
-      className="flex justify-center items-center overflow-hidden bg-cyan-100"
+      className="flex justify-center items-center overflow-hidden accent-gray-400"
       style={{
         height: viewportHeight,
         width: "100%",
