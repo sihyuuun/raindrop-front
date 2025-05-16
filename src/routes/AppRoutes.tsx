7341a6db1a290be6276603d097787491c2e11b2b
jihoi0615 <pjhalice0615@gmail.com>
2025-05-16 10:34:47 +0900

    #94 feat: AppRoutes develop 상태로 통일

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,7 +3,6 @@ import { ScenePage } from "@/pages/ScenePage";
 import KakaoAuthCallback from "@/pages/KakaoAuthCallback.tsx";
 import SceneRedirector from "@/components/common/SceneRedirector";
 import { MessagePage } from "@/pages/MessagePage";
-import TestDeleteModal from "@/pages/TestDeleteModal";
 
 const AppRoutes = () => {
   return (
@@ -13,7 +12,6 @@ const AppRoutes = () => {
         <Route path="/:encryptedSceneId" element={<ScenePage />} />
         <Route path="/auth/login/kakao" element={<KakaoAuthCallback />} />
         <Route path="/message" element={<MessagePage />} />
-        <Route path="/test-delete-modal" element={<TestDeleteModal />} />
       </Routes>
     </>
   );
