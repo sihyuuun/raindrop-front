eb18497076103773ec39522d700c201affee2ad1
sihyuuun <leesh3597@gmail.com>
2025-05-16 17:29:53 +0900

    #104 refactor: AppRoutes에서 SceneRedirector 삭제

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,13 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import { ScenePage } from "@/pages/ScenePage";
 import KakaoAuthCallback from "@/pages/KakaoAuthCallback.tsx";
-import SceneRedirector from "@/components/common/SceneRedirector";
 import { MessagePage } from "@/pages/MessagePage";
 
 const AppRoutes = () => {
   return (
     <>
-      <SceneRedirector />
       <Routes>
         <Route path="/:encryptedSceneId" element={<ScenePage />} />
         <Route path="/auth/login/kakao" element={<KakaoAuthCallback />} />
